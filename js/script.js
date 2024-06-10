e953fc9419f040b93a831a664bd6e766c6a4e8e4
N-Vaughn <nvaughn@eastsideprep.org>
2024-06-10 12:16:04 -0700

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-//alert("javascript works")
-function showImage(){
-    var image=document.getElementById('kobe');
-    image.src="images/kobe_bryant_olympic_wp.jpg";
-}
-function showImage(){
-    var image=document.getElementById('Spotify_QR_Code');
-    image.src="images/spotify_code_prayforme_kendricklamar_theweeknd.jpeg";
-}
